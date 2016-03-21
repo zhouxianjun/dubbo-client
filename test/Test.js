57c61483830da3d96d7f7600ddb0ac37fd362c69
zhouxianjun <gary@cn-face.com>
2016-03-21 15:45:45 +0800

    init

---
+++ test/Test.js
@@ -0,0 +1,29 @@
+/**
+ * Created with JetBrains Idea.
+ * User: Gary
+ * Date: 2016/3/21
+ * Time: 14:00
+ *                 _ooOoo_
+ *                o8888888o
+ *                88" . "88
+ *                (| -_- |)
+ *                O\  =  /O
+ *             ____/`---'\____
+ *           .'  \\|     |//  `.
+ *           /  \\|||  :  |||//  \
+ *           /  _||||| -:- |||||-  \
+ *           |   | \\\  -  /// |   |
+ *           | \_|  ''\---/''  |   |
+ *           \  .-\__  `-`  ___/-. /
+ *         ___`. .'  /--.--\  `. . __
+ *      ."" '<  `.___\_<|>_/___.'  >'"".
+ *     | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+ *     \  \ `-.   \_ __\ /__ _/   .-` /  /
+ *======`-.____`-.___\_____/___.-`____.-'======
+ *                   `=---='
+ *^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ *           佛祖保佑       永无BUG
+ */
+'use strict';
+const AbstractRegistry = require('../lib/registry/AbstractRegistry');
+new AbstractRegistry('multicast://224.5.6.7:1234/com.alibaba.dubbo.registry.RegistryService?application=demo-provider&backup=224.5.6.7:5678&dubbo=2.0.0&interface=com.alibaba.dubbo.registry.RegistryService&owner=william&pid=10616&timestamp=1458543365471');
