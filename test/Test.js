bfbeaf071dceb6f05af405a7a4c2dbc4a1cf7955
zhouxianjun <gary@cn-face.com>
2016-03-24 15:07:50 +0800

    URL 对象 封装 AbstractRegistry 对象 封装 提供 UrlUtils 工具

--- test/Test.js
+++ test/Test.js
@@ -25,5 +25,19 @@
  *           佛祖保佑       永无BUG
  */
 'use strict';
-const AbstractRegistry = require('../lib/registry/AbstractRegistry');
-new AbstractRegistry('multicast://224.5.6.7:1234@aaa:sss/com.alibaba.dubbo.registry.RegistryService?application=demo-provider&backup=224.5.6.7:5678&dubbo=2.0.0&interface=com.alibaba.dubbo.registry.RegistryService&owner=william&pid=10616&timestamp=1458543365471');
+//const AbstractRegistry = require('../lib/registry/AbstractRegistry');
+//new AbstractRegistry('multicast://224.5.6.7:1234@aaa:sss/com.alibaba.dubbo.registry.RegistryService?application=demo-provider&backup=224.5.6.7:5678&dubbo=2.0.0&interface=com.alibaba.dubbo.registry.RegistryService&owner=william&pid=10616&timestamp=1458543365471');
+
+const co = require('co');
+const URL = require('../lib/registry/URL');
+let u = new URL({
+    protocol: 'http',
+    host: 'www.cn-face.com',
+    port: 8888,
+    path: 'com.alibaba.dubbo.registry.RegistryService',
+    parameters: {
+        application: 'demo-provider'
+    }
+});
+
+console.log(u.toString());
