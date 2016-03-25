534d03732d75176a3d85e77b83c71229431f53c3
zhouxianjun <gary@cn-face.com>
2016-03-25 09:33:59 +0800

    添加注册器的实现FailbackRegistry

--- test/Test.js
+++ test/Test.js
@@ -27,7 +27,24 @@
 'use strict';
 //const AbstractRegistry = require('../lib/registry/AbstractRegistry');
 //new AbstractRegistry('multicast://224.5.6.7:1234@aaa:sss/com.alibaba.dubbo.registry.RegistryService?application=demo-provider&backup=224.5.6.7:5678&dubbo=2.0.0&interface=com.alibaba.dubbo.registry.RegistryService&owner=william&pid=10616&timestamp=1458543365471');
-
+const logger = require('tracer').dailyfile({
+    root:'../logs',
+    format : [
+        "{{timestamp}} <{{title}}>  [{{file}}:{{line}}:{{pos}}] - {{message}}", //default format
+        {
+            error : "{{timestamp}} <{{title}}>  [{{file}}:{{line}}:{{pos}}] - {{message}}\nCall Stack:\n{{stack}}" // error format
+        }
+    ],
+    dateformat : "HH:MM:ss.L",
+    preprocess :  function(data){
+        data.title = data.title.toUpperCase();
+    },
+    transport: function(data){
+        console.log(data.output);
+    }
+});
+const util = require('util');
+const ring = require('ring');
 const co = require('co');
 const URL = require('../lib/registry/URL');
 let u = new URL({
@@ -41,3 +58,6 @@ let u = new URL({
 });
 
 console.log(u.toString());
+
+let a = new Promise(function(success, fail){});
+console.log(ring.instance(a, Promise));
